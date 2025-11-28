56d5b77a7f17e58ff5a26fdfe84af431580261bd
daffa1225 <mdaffarizqyramadhan_2327240130@mhs.mdp.ac.id>
2025-11-28 16:56:02 +0700

    pertemuan

--- griya-mdp/griya-mdp/src/app/login/login.ts
+++ griya-mdp/griya-mdp/src/app/login/login.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';  // ✅ Tambah Router
+import { AuthService } from '../services/auth.service';  // ✅ Import AuthService
 
 @Component({
   selector: 'app-login',
